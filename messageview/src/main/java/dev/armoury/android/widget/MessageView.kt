4951625f9b52d269b32417e4b2dbfd2157db47a9
Majid <m.khosravi@farakav.com>
2022-08-23 17:32:42 +0430

    We've removed a TODO comment

--- messageview/src/main/java/dev/armoury/android/widget/MessageView.kt
+++ messageview/src/main/java/dev/armoury/android/widget/MessageView.kt
@@ -285,7 +285,7 @@ open class MessageView @JvmOverloads constructor(
             States.HIDE -> visibility = View.GONE
         }
         binding.animation.updateState(state, lottieFileName)
-        binding.button.updateState()
+        binding.button.updateState() //
         this.visibility = visibility
         binding.progress.visibility = loadingIndicatorVisibility
     }
