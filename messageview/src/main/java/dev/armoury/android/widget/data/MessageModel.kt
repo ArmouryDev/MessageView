a5ee87c5faf2348eaca71a4863d31d2fbe599a80
Majid <m.khosravi@farakav.com>
2022-08-25 17:03:48 +0430

    We've changed some fields modifires

--- messageview/src/main/java/dev/armoury/android/widget/data/MessageModel.kt
+++ messageview/src/main/java/dev/armoury/android/widget/data/MessageModel.kt
@@ -1,5 +1,6 @@
 package dev.armoury.android.widget.data
 
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import dev.armoury.android.widget.MessageView
@@ -7,14 +8,17 @@ import dev.armoury.android.widget.MessageView
 internal const val INVALID_VALUE = -1
 
 class MessageModel(
-    @MessageView.State val state: Int,
+    @MessageView.State var state: Int,
     @DrawableRes var imageRes: Int = INVALID_VALUE,
     @StringRes var titleTextRes: Int = INVALID_VALUE,
     var titleText: String? = null,
     @StringRes var descriptionTextRes: Int = INVALID_VALUE,
     var descriptionText: String? = null,
     @StringRes var buttonTextRes: Int = INVALID_VALUE,
-    var buttonText: String? = null
+    var buttonText: String? = null,
+    @ColorRes
+    var buttonColor: Int = INVALID_VALUE,
+    var remainedTime : Long = 0
 ) {
 
     fun reset() {
@@ -25,6 +29,8 @@ class MessageModel(
         descriptionText = null
         buttonTextRes = INVALID_VALUE
         buttonText = null
+        buttonColor = INVALID_VALUE
+        remainedTime = 0
     }
 
     fun hasTitle() = titleText != null || titleTextRes != INVALID_VALUE
